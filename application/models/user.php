aff2ca4f30545ac828eb5e077eaf60c155926831
Phat Wangrungarun <phatograph@gmail.com>
2012-06-04 14:50:24 +0700

    Sample user model

---
+++ application/models/user.php
@@ -0,0 +1,14 @@
+<?php
+class User extends Doctrine_Record {
+
+  public function setTableDefinition() {
+    $this->hasColumn('username', 'string', 255, array('unique' => 'true'));
+    $this->hasColumn('password', 'string', 255);
+    $this->hasColumn('email', 'string', 255, array('unique' => 'true'));
+  }
+
+  public function setUp() {
+    $this->setTableName('user');
+    $this->actAs('Timestampable');
+  }
+}
